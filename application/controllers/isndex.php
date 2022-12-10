c0cbbe39ed2c2f21f11782145d76fc7bb6317684
Miftahul Ulum <ulumiftahul06@gmail.com>
2022-12-10 18:28:06 +0700

    debug classanem

--- application/controllers/isndex.php
+++ application/controllers/isndex.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Index extends CI_Controller
+class Index extends BaseController
 {
 
     public function __construct()
