a20af0430479b8753ac72e8e189e800d77393f48
Miftahul Ulum <ulumiftahul06@gmail.com>
2022-12-10 18:36:01 +0700

    debug ulang

--- application/views/template/app_top.php
+++ application/views/template/app_top.php
@@ -68,7 +68,7 @@
 
 <body>
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <!-- <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="<?= site_url('index') ?>">
             SIPMB AMD Academy<br>
             <sub>By: <?= AUTHOR_NAME ?></sub>
@@ -82,7 +82,7 @@
                 <a class="nav-link px-3" href="#">Sign out</a>
             </div>
         </div>
-    </header>
+    </header> -->
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
